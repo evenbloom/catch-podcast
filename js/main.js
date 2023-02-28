04251a9ccf1124fc5fd10621d569c0ff4530b273
evenbloom <hello@evenbloom.com>
2023-02-28 16:51:30 +0000

    main update js-

--- js/main.js
+++ js/main.js
@@ -1,87 +1,89 @@
-const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-const html = document.querySelector("html");
-const toggleSwitch = document.createElement("div");
-toggleSwitch.classList.add("toggle-switch");
+ const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+const isThemeDark = () => localStorage.getItem("theme") === "dark";
 
-const createSVGWrapper = () => {
+function setTheme(theme) {
+  const html = document.querySelector("html");
+  html.classList.toggle("theme-default", theme === "light");
+  html.classList.toggle("theme-blackout", theme === "dark");
+  localStorage.setItem("theme", theme);
+  console.info(`${theme} mode activated.`);
+}
+
+function toggleThemeMode() {
+  setTheme(isThemeDark() ? "light" : "dark");
+}
+
+function createSVGWrapper() {
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute("viewBox", "0 0 24 24");
   svg.setAttribute("fill", "currentColor");
   return svg;
 }
 
-const createIcon = (path) => {
+function createIcon(pathData, circle) {
   const svg = createSVGWrapper();
-  const icon = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  icon.setAttribute("d", path);
-  svg.appendChild(icon);
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("d", pathData);
+  if (circle) {
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute("cx", "12");
+    circle.setAttribute("cy", "12");
+    circle.setAttribute("r", "5");
+    svg.appendChild(circle);
+  }
+  svg.appendChild(path);
   return svg;
 }
 
-const setTheme = (theme) => {
-  html.classList.add(`theme-${theme}`);
-  html.classList.remove(`theme-${theme === "dark" ? "default" : "blackout"}`);
-  localStorage.setItem("theme", theme);
-  console.info(`${theme === "dark" ? "Dark" : "Light"} mode activated.`);
-}
-
-const toggleThemeMode = () => {
-  const currentTheme = localStorage.getItem("theme");
-  const theme = currentTheme === "dark" || !currentTheme && darkModeMediaQuery.matches ? "light" : "dark";
-  setTheme(theme);
-}
-
-const addToggleSwitcher = () => {
-  const darkModeIcon = createIcon("M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z");
-  const lightModeIcon = createIcon("M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42");
-  toggleSwitch.appendChild(darkModeIcon);
-  toggleSwitch.appendChild(lightModeIcon);
+function createToggleSwitch() {
+  const toggleSwitch = document.createElement("div");
+  toggleSwitch.classList.add("toggle-switch");
+  toggleSwitch.innerHTML = `
+    <div class="dark-mode-icon">
+      ${createIcon("M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z")}
+    </div>
+    <div class="light-mode-icon">
+      ${createIcon("M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42", true)}
+    </div>
+  `;
   toggleSwitch.addEventListener("click", toggleThemeMode);
-  
+  return toggleSwitch;
+}
+
+function addToggleSwitcher() {
   const navbar = document.querySelector(".super-navbar");
   if (navbar) {
-    if (!navbar.querySelector(".toggle-switch")) {
-      navbar.appendChild(toggleSwitch);
-    }
-    console.info("The theme switcher was already added.");
+    const toggleSwitch = createToggleSwitch();
+    navbar.append(toggleSwitch);
+    console.info("The theme switcher was added.");
   } else {
     console.info("You need to add a navbar to the page.");
   }
 }
 
-const currentTheme = localStorage.getItem("theme");
-if (currentTheme) {
-  setTheme(currentTheme);
-} else if (darkModeMediaQuery.matches) {
-  setTheme("dark");
-} else {
-  setTheme("default");
+function onPageLoad() {
+  const notionHeader = document.querySelector(".notion-header");
+  if (!notionHeader) return;
+  
+  const removeFooterCover = () => {
+    const footerCover = document.querySelector(".footer-cover");
+    if (footerCover) footerCover.remove();
+  };
+  
+  removeFooterCover();
+  
+  new MutationObserver(function (mutations) {
+    for (const mutation of mutations) {
+      if (mutation.type === "characterData") {
+        removeFooterCover();
+      }
+    }
+  }).observe(notionHeader, {subtree: true, characterData: true});
 }
 
-addToggleSwitcher();
-
-function onPageLoad() {
-   const e = document.querySelector(".notion-header");
-
-  const removeFooterCover = () => {
-    const e = document.querySelector(".footer-cover");
-    e && e.remove();
-  };
-
-  removeFooterCover();
-
-  new MutationObserver(function (e, t) {
-    for (const t of e)
-      "characterData" === t.type && removeFooterCover();
-  }).observe(e, {
-    subtree: !0,
-    characterData: !0,
-  });
+function setInitialTheme() {
+  setTheme(isThemeDark() || (!localStorage.getItem("theme") && darkModeMediaQuery.matches) ? "dark" : "light");
 }
 
 setInitialTheme();
-window.onload = function () {
-  addToggleSwitcher();
-};
-
-document.addEventListener("DOMContentLoaded", onPageLoad);
+window.addEventListener("load", addToggleSwitcher);
