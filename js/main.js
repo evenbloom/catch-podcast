45c169b2da33fe01eb8bc633cf70237daaea6f5c
evenbloom <hello@evenbloom.com>
2023-02-28 17:39:29 +0000

    navbar toggle switch

--- js/main.js
+++ js/main.js
@@ -20,44 +20,55 @@ function createSVGWrapper() {
   return svg;
 }
 
-function createIcon(pathData, circle) {
+function createDarkModeIcon() {
   const svg = createSVGWrapper();
   const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  path.setAttribute("d", pathData);
-  if (circle) {
-    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    circle.setAttribute("cx", "12");
-    circle.setAttribute("cy", "12");
-    circle.setAttribute("r", "5");
-    svg.appendChild(circle);
-  }
+  path.setAttribute("d", "M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z");
   svg.appendChild(path);
   return svg;
 }
 
+function createLightModeIcon() {
+    const svg = createSVGWrapper();
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", "M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42");
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute("cx", "12");
+    circle.setAttribute("cy", "12");
+    circle.setAttribute("r", "5");
+    svg.appendChild(path);
+    svg.appendChild(circle);
+    svg.setAttribute("fill", "none");
+    svg.setAttribute("stroke", "#fff");
+    svg.setAttribute("stroke-width", "2");
+    svg.setAttribute("stroke-linecap", "round");
+    svg.setAttribute("stroke-linejoin", "round");
+    return svg;
+ }
+
 function createToggleSwitch() {
   const toggleSwitch = document.createElement("div");
   toggleSwitch.classList.add("toggle-switch");
-  toggleSwitch.innerHTML = `
-    <div class="dark-mode-icon">
-      ${createIcon("M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z")}
-    </div>
-    <div class="light-mode-icon">
-      ${createIcon("M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42", true)}
-    </div>
-  `;
+  const darkModeIcon = document.createElement("div");
+  darkModeIcon.classList.add("dark-mode-icon");
+  const lightModeIcon = document.createElement("div");
+  lightModeIcon.classList.add("light-mode-icon");
+  toggleSwitch.appendChild(darkModeIcon);
+  toggleSwitch.appendChild(lightModeIcon);
+  darkModeIcon.appendChild(createDarkModeIcon());
+  lightModeIcon.appendChild(createLightModeIcon());
   toggleSwitch.addEventListener("click", toggleThemeMode);
   return toggleSwitch;
 }
 
 function addToggleSwitcher() {
-  const navbar = document.querySelector(".super-navbar");
-  if (navbar) {
+  const superNavbar = document.querySelector(".super-navbar");
+  if (superNavbar) {
     const toggleSwitch = createToggleSwitch();
-    navbar.append(toggleSwitch);
+    superNavbar.appendChild(toggleSwitch);
     console.info("The theme switcher was added.");
   } else {
-    console.info("You need to add a navbar to the page.");
+    console.info("You need to add a super navbar to the page.");
   }
 }
 
