f7b6e7f8739fac069e58440ddfb4f2d6358ae4cf
evenbloom <hello@evenbloom.com>
2023-02-28 16:35:23 +0000

    main update js

--- js/main.js
+++ js/main.js
@@ -1,78 +1,64 @@
- var darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)")
-  , getCurrentTheme = ()=>localStorage.getItem("theme")
-  , saveThemeInLocalStorage = e=>localStorage.setItem("theme", e)
-  , isThemeDark = ()=>"dark" === getCurrentTheme()
-  , isThemeLight = ()=>"light" === getCurrentTheme();
+const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+const html = document.querySelector("html");
+const toggleSwitch = document.createElement("div");
+toggleSwitch.classList.add("toggle-switch");
 
-function setLightMode() {
-    document.querySelector("html").classList.add("theme-default"),
-    document.querySelector("html").classList.remove("theme-blackout"),
-    saveThemeInLocalStorage("light"),
-    console.info("Light mode activated.")
-}
-function setDarkMode() {
-    document.querySelector("html").classList.add("theme-blackout"),
-    document.querySelector("html").classList.remove("theme-default"),
-    saveThemeInLocalStorage("dark"),
-    console.info("Dark mode activated.")
-}
-function setInitialTheme() {
-    "dark" === getCurrentTheme() || !getCurrentTheme() && darkModeMediaQuery.matches ? setDarkMode() : setLightMode()
-}
-function toggleThemeMode() {
-    var e = getCurrentTheme();
-    "dark" === e || !e && darkModeMediaQuery.matches ? setLightMode() : setDarkMode()
-}
-function createSVGWrapper() {
-    var e = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    return e.setAttribute("viewBox", "0 0 24 24"),
-    e.setAttribute("fill", "currentColor"),
-    e
-}
-function createDarkModeIcon() {
-    var e = createSVGWrapper()
-      , t = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    return t.setAttribute("d", "M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"),
-    e.appendChild(t),
-    e
-}
-function createLightModeIcon() {
-    var e = createSVGWrapper()
-      , t = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    return circle = document.createElementNS("http://www.w3.org/2000/svg", "circle"),
-    t.setAttribute("d", "M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42"),
-    e.setAttribute("fill", "none"),
-    e.setAttribute("stroke", "#fff"),
-    e.setAttribute("stroke-width", "2"),
-    e.setAttribute("stroke-linecap", "round"),
-    e.setAttribute("stroke-linejoin", "round"),
-    circle.setAttribute("cx", "12"),
-    circle.setAttribute("r", "5"),
-    circle.setAttribute("cy", "12"),
-    e.appendChild(t),
-    e.appendChild(circle),
-    e
-}
-function createToggleSwitch() {
-    var e = document.createElement("div");
-    e.classList.add("toggle-switch");
-    var t = document.createElement("div");
-    t.classList.add("dark-mode-icon");
-    var r = document.createElement("div");
-    return r.classList.add("light-mode-icon"),
-    e.appendChild(t),
-    e.appendChild(r),
-    t.appendChild(createDarkModeIcon()),
-    r.appendChild(createLightModeIcon()),
-    e
-}
-function addToggleSwitcher() {
-    var e = createToggleSwitch()
-      , t = document.querySelector(".super-navbar");
-    t ? t.querySelector(".toggle-switch") ? console.info("The theme switcher was already added.") : (t.append(e),
-    e.addEventListener("click", toggleThemeMode)) : console.info("You need to add a navbar to the page.")
+const createSVGWrapper = () => {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("viewBox", "0 0 24 24");
+  svg.setAttribute("fill", "currentColor");
+  return svg;
 }
 
+const createIcon = (path) => {
+  const svg = createSVGWrapper();
+  const icon = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  icon.setAttribute("d", path);
+  svg.appendChild(icon);
+  return svg;
+}
+
+const setTheme = (theme) => {
+  html.classList.add(`theme-${theme}`);
+  html.classList.remove(`theme-${theme === "dark" ? "default" : "blackout"}`);
+  localStorage.setItem("theme", theme);
+  console.info(`${theme === "dark" ? "Dark" : "Light"} mode activated.`);
+}
+
+const toggleThemeMode = () => {
+  const currentTheme = localStorage.getItem("theme");
+  const theme = currentTheme === "dark" || !currentTheme && darkModeMediaQuery.matches ? "light" : "dark";
+  setTheme(theme);
+}
+
+const addToggleSwitcher = () => {
+  const darkModeIcon = createIcon("M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z");
+  const lightModeIcon = createIcon("M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42");
+  toggleSwitch.appendChild(darkModeIcon);
+  toggleSwitch.appendChild(lightModeIcon);
+  toggleSwitch.addEventListener("click", toggleThemeMode);
+  
+  const navbar = document.querySelector(".super-navbar");
+  if (navbar) {
+    if (!navbar.querySelector(".toggle-switch")) {
+      navbar.appendChild(toggleSwitch);
+    }
+    console.info("The theme switcher was already added.");
+  } else {
+    console.info("You need to add a navbar to the page.");
+  }
+}
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  setTheme(currentTheme);
+} else if (darkModeMediaQuery.matches) {
+  setTheme("dark");
+} else {
+  setTheme("default");
+}
+
+addToggleSwitcher();
 
 function onPageLoad() {
    const e = document.querySelector(".notion-header");
